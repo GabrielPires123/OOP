47a9acc094464170530b0a2b975f9d9285000d81
Gabriel Pires <139234730+GabrielPires123@users.noreply.github.com>
2025-02-04 14:47:06 -0300

    Delete

--- Banco de dados treino/Aplication/Main.java
+++ Banco de dados treino/Aplication/Main.java
@@ -1,36 +1,38 @@
 package Aplication;
 
 import db.DB;
-import db.DBexception;
+import db.DbIntegrityException;
+import java.sql.*;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class Main {
     public static void main(String[] args) {
 
+
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
-
-        try
-        {
-
+        PreparedStatement st = null;
+        try {
             conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department,seller");
 
-            while(rs.next())
-            {
-                System.out.println("Id:"+ rs.getInt("Id") + "," + " departamento:"+rs.getString("Name")+ " Pessoa: "+rs.getString("Name"));
-            }
+            st = conn.prepareStatement(
+                    "DELETE FROM department "
+                            + "WHERE "
+                            + "Id = ?");
 
-        } catch (SQLException e) {
-            throw new DBexception(e.getMessage());
+            st.setInt(1, 5);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
         }
-
+        catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
     }
 }
